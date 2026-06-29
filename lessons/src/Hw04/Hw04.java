d889594b0d91474ebbc4d4974485d222a536173e
Atrhur <arturkozak2704@gmail.com>
2026-06-29 21:21:02 +0300

    Hw4- update

--- lessons/src/Hw04/Hw04.java
+++ lessons/src/Hw04/Hw04.java
@@ -2,6 +2,6 @@ package Hw04;
 
 public class Hw04 {
     public static void main(String[] args) {
-        System.out.println("This is shop-online app");
+        System.out.println("Shop-online for customers");
     }
 }
