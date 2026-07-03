c876cce7cbd8142e62da44837e49f8c17e302aaa
Atrhur <arturkozak2704@gmail.com>
2026-07-03 17:24:19 +0300

    Hw5

---
+++ lessons/src/app/Main.java
@@ -0,0 +1,32 @@
+package app;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        int clients = 1;
+        String product = "Smartphone";
+        double price = 2430.68;
+        int days = 5;
+        int sales = 5;
+
+        double total = sales * price;
+
+        double DailySales = total / days;
+
+        System.out.printf("Product No 1: %s,%ntotal sales for %d days is EUR %.2f,%nsales by day is EUR %.2f.%n",
+                product, days, total, DailySales);
+
+        clients = 2;
+        product = "laptop";
+        price = 1498.12;
+        days = 7;
+        sales = 7;
+
+        double total2 = sales * price;
+
+        double DailySales2 = total / days;
+        System.out.printf("Product No 2: %s,%ntotal sales for %d days is EUR %.2f,%nsales by day is EUR %.2f.%n",
+                product, days, total2, DailySales2);
+    }
+}
