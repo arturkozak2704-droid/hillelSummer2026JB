bc4c16d4af1781530c760a582cbb3a7bd799a648
Atrhur <arturkozak2704@gmail.com>
2026-07-17 17:31:37 +0300

    Hw8

--- lessons/src/app2/Main.java
+++ lessons/src/app2/Main.java
@@ -1,13 +1,69 @@
 package app2;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        int Num = 0;
-        for(int i = 1; i <= 6; i++){
-            Num += i;
-            System.out.println("Num is " + i + ", sum is " + Num);
+        int[] array = new int[20];
+        int sumNegative = 0;
+        int even = 0;
+        int odd = 0;
+        int min = array[0];
+        int max = array[0];
+        int minIndex = 0;
+        int maxIndex = 0;
+        int firstIndexNegative = -1;
+        int sum = 0;
+        int count = 0;
+        double averageAfter = 0;
+        Random random = new Random();
+        System.out.print("Елементи масиву: ");
+
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(-100, 101);
+            System.out.print(array[i] + " ");
+            if (array[i] < 0) {
+                sumNegative += array[i];
+            }
+            if (array[i] % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+            System.out.println();
+            if (array[i] < min) {
+                min = array[i];
+                minIndex = i;
+            }
+            if (array[i] > max) {
+                max = array[i];
+                maxIndex = i;
+            }
+            if (array[i] < 0) {
+                if (firstIndexNegative == -1) {
+                    firstIndexNegative = i;
+                }
+            }
+
         }
-        System.out.println("-------------");
-        System.out.println("Sum of numbers is " + Num);
+        if (firstIndexNegative != -1) {
+            for (int i = firstIndexNegative + 1; i < array.length; i++) {
+                sum += array[i];
+                count++;
+            }
+            if(count > 0) {
+                averageAfter = (double) sum / count;
+            }
+        }
+
+        System.out.println();
+        System.out.println("Сума від'ємних чисел: " + sumNegative);
+        System.out.println("Кількість парних чисел: " + even);
+        System.out.println("Кількість непарних чисел: " + odd);
+        System.out.println("Найменший елемент: " + min + " (з індексом " + minIndex + ")");
+        System.out.println("Найбільший елемент: " + max + " (з індексом " + maxIndex + ")");
+        System.out.println("Середнє арифметичне чисел після першого від'ємного числа: " + averageAfter);
     }
+
 }
